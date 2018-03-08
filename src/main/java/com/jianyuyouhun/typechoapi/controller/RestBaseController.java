aad5b0aa49a4e64c6e791e40a80af11e1f65639d
wangyu <1837960253@qq.com>
2018-03-08 16:35:31 +0800

    修改请求基类和枚举

--- src/main/java/com/jianyuyouhun/typechoapi/controller/RestBaseController.java
+++ src/main/java/com/jianyuyouhun/typechoapi/controller/RestBaseController.java
@@ -1,7 +1,9 @@
 package com.jianyuyouhun.typechoapi.controller;
 
+import com.jianyuyouhun.typechoapi.utils.json.JsonUtil;
 import org.json.JSONObject;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 /**
@@ -11,11 +13,28 @@ public abstract class RestBaseController {
 
     protected JSONObject buildResult(RestResult restResult, Object object) {
         if (object == null) {
-            return RestResult.buildDefaultJson(restResult);
+            return buildDefaultJson(restResult);
         } else if (object instanceof List) {
-            return RestResult.build(restResult, (List<Object>) object);
+            return build(restResult, (List<Object>) object);
         } else {
-            return RestResult.build(restResult, object);
+            return build(restResult, object);
         }
+    }
+
+    private JSONObject buildDefaultJson(@NotNull RestResult restResult) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("code", restResult.code);
+        jsonObject.put("message", restResult.message);
+        return jsonObject;
+    }
+
+    private JSONObject build(@NotNull RestResult restResult, Object object) {
+        return buildDefaultJson(restResult)
+                .put("data", JsonUtil.toJSONObject(object));
+    }
+
+    private JSONObject build(@NotNull RestResult restResult, List<Object> list) {
+        return buildDefaultJson(restResult)
+                .put("list", JsonUtil.toJSONArray(list));
     }
 }
--- src/main/java/com/jianyuyouhun/typechoapi/controller/RestResult.java
+++ src/main/java/com/jianyuyouhun/typechoapi/controller/RestResult.java
@@ -1,10 +1,5 @@
 package com.jianyuyouhun.typechoapi.controller;
 
-import com.jianyuyouhun.typechoapi.utils.json.JsonUtil;
-import org.json.JSONObject;
-
-import java.util.List;
-
 public enum RestResult {
     RESULT_DATA_OK(200, "RESULT_DATA_OK"),
     RESULT_DATA_EMPTY(201, "RESULT_DATA_EMPTY"),
@@ -12,27 +7,10 @@ public enum RestResult {
     RESULT_SYS_ERROR(203, "RESULT_SYS_ERROR"),
     RESULT_USER_NOT_EXIST(101, "USER_NOT_EXIST"),
     RESULT_PWD_NOT_MATCH(102, "PWD_NOT_MATCH");
-    private int code;
-    private String message;
+    protected int code;
+    protected String message;
     RestResult(int code, String msg) {
         this.code = code;
         this.message = msg;
-    }
-
-    public static JSONObject buildDefaultJson(RestResult restResult) {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("code", restResult.code);
-        jsonObject.put("message", restResult.message);
-        return jsonObject;
-    }
-
-    public static JSONObject build(RestResult restResult, Object object) {
-        return buildDefaultJson(restResult)
-                .put("data", JsonUtil.toJSONObject(object));
-    }
-
-    public static JSONObject build(RestResult restResult, List<Object> list) {
-        return buildDefaultJson(restResult)
-                .put("list", JsonUtil.toJSONArray(list));
     }
 }
